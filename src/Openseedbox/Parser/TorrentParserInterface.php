95bb7942aae3020d3b8daa4582904bd5ab1b36a1
Erin Drummond <erin@flavouroom.com>
2014-01-08 16:49:31 +1300

    Implemented torrent parser and updated unit tests

---
+++ src/Openseedbox/Parser/TorrentParserInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Openseedbox\Parser;
+
+interface TorrentParserInterface {
+
+	/**
+	 * Create a data structure from torrent metadata
+	 * @param string $data The torrent metadata
+	 * @return Openseedbox\Parser\TorrentInterface
+	 */
+	public function parse($data);
+
+}
